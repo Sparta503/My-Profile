a522c2de4a9eb2e6b94c908ee373b3d2224e109d
DiorHomme <chomunodisaa68@gmail.com>
2025-02-24 21:59:26 +0200

    summary

---
+++ My-profile/src/assets/index.ts
@@ -0,0 +1,62 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./firebase.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./bootstrap.jpg";
+
+import meta from "./cloud.jpg";
+import shopify from "./netone.png";
+import starbucks from "./Telone-logo.jpg";
+import tesla from "./Nedbank.png";
+
+import carrent from "./findMyKey.jpg";
+import jobit from "./converse.jpg";
+import tripguide from "./Groovy.jpg";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
